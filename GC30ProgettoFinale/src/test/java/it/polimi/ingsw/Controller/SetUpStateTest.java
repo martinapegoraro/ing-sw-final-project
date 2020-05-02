79c64d0fcd2c0f19cb6bf13f1ba626bf94207bc0
Matteo <matteo9.panzeri@mail.polimi.it>
2020-05-02 14:10:43 +0200

    test per setUpState

---
+++ GC30ProgettoFinale/src/test/java/it/polimi/ingsw/Controller/SetUpStateTest.java
@@ -0,0 +1,92 @@
+package it.polimi.ingsw.Controller;
+
+import it.polimi.ingsw.Model.Block;
+import it.polimi.ingsw.Model.Exceptions.*;
+import it.polimi.ingsw.Model.Model;
+import it.polimi.ingsw.Utils.Choice;
+import it.polimi.ingsw.Utils.InitialPositionChoice;
+import it.polimi.ingsw.Utils.MoveChoice;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SetUpStateTest {
+
+    State stateUnderTest;
+    Model model;
+
+
+    @Before
+    public void stateSetUp()
+    {
+
+        List<String> listaNomi=new ArrayList<>();
+        listaNomi.add("pippo");
+        listaNomi.add("pluto");
+        listaNomi.add("paperino");
+        model=new Model(listaNomi);
+        stateUnderTest=new SetUpState(model);
+    }
+
+    @Test
+    public void getIDTest()
+    {
+        assertEquals(StateEnum.SetUp,stateUnderTest.getID());
+    }
+
+    @Test
+    public void startUpTest()
+    {
+        stateUnderTest.startup(model);
+        assertEquals(model.getTurn().getCurrentPlayer().getPlayerName(),model.getTurn().getPlayersList().get(0).getPlayerName());
+
+    }
+
+    @Test(expected = BoxAlreadyOccupiedException.class)
+    public void updateTestWithOccupiedBox() throws BoxAlreadyOccupiedException, BuildErrorException, WrongChoiceTypeException, MoveErrorException, TowerCompleteException {
+        Choice c= new InitialPositionChoice(1,1);
+        model.getTurn().getBoardInstance().getBox(1,1).getTower().build(Block.DOME);
+        stateUnderTest.update(c,model);
+    }
+
+
+    @Test(expected = WrongChoiceTypeException.class)
+    public void updateTestWithWrongChoice() throws BoxAlreadyOccupiedException, BuildErrorException, WrongChoiceTypeException, MoveErrorException {
+        Choice c= new MoveChoice(1,1);
+        stateUnderTest.update(c,model);
+    }
+
+    @Test
+    public void updateTest()
+    {
+        Choice c= new MoveChoice(1,1);
+        try {
+            stateUnderTest.update(c,model);
+        } catch (WrongChoiceTypeException e) {
+            e.printStackTrace();
+        } catch (MoveErrorException e) {
+            e.printStackTrace();
+        } catch (BuildErrorException e) {
+            e.printStackTrace();
+        } catch (BoxAlreadyOccupiedException e) {
+            e.printStackTrace();
+        }
+        Choice c1= new MoveChoice(3,1);
+        try {
+            stateUnderTest.update(c1,model);
+        } catch (WrongChoiceTypeException e) {
+            e.printStackTrace();
+        } catch (MoveErrorException e) {
+            e.printStackTrace();
+        } catch (BuildErrorException e) {
+            e.printStackTrace();
+        } catch (BoxAlreadyOccupiedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(stateUnderTest.hasFinished());
+    }
+}
