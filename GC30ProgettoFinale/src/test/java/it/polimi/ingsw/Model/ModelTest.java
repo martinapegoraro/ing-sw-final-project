af10affb9ff1070557786cd612a219cf0db78935
martinapegoraro <61741320+martinapegoraro@users.noreply.github.com>
2020-05-04 23:21:22 +0200

    Finito Test per Model

--- GC30ProgettoFinale/src/test/java/it/polimi/ingsw/Model/ModelTest.java
+++ GC30ProgettoFinale/src/test/java/it/polimi/ingsw/Model/ModelTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Model;
 
+import it.polimi.ingsw.Controller.BeginTurnState;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,30 +11,26 @@ import static org.junit.Assert.*;
 
 public class ModelTest {
     Model modelTest;
-    ModelRepresentation modelRepTest;
-    Board boardTest;
-    Player player1, player2;
-
+    BeginTurnState beginTurnState;
+    Box b1, b2, b3, b4, b5;
+    Board instanceBoard;
 
     @Before
 
     public void initializeModel ()
     {
-        boardTest = Board.getInstance();
+        instanceBoard = Board.getInstance();
         List<String> listaNomi=new ArrayList<>();
         listaNomi.add("pippo");
         listaNomi.add("pluto");
         listaNomi.add("paperino");
         modelTest =new Model(listaNomi);
-        player1 = new Player(1, "Anna");
-        player2 = new Player (2, "Marco");
-        List<Player> players = new ArrayList<>();
-        players.add(player1);
-        players.add(player2);
-        int[][] selectedCells = new int[5][5];
-        modelRepTest = new ModelRepresentation(boardTest, players,selectedCells);
-
-
+        beginTurnState = new BeginTurnState();
+        b1 = instanceBoard.getBox(1,2);
+        b2 = instanceBoard.getBox(0,3);
+        b3 = instanceBoard.getBox(2,2);
+        b4 = instanceBoard.getBox(3,1);
+        b5 = instanceBoard.getBox(4,1);
     }
 
     @Test
@@ -48,5 +45,43 @@ public class ModelTest {
     public void createModelRep()
     {
         assertNotNull(modelTest.getModelRep());
+
+        assertEquals(3, modelTest.getModelRep().getPlayerNum());
+    }
+
+
+    @Test
+    public void updateModelRep()
+    {
+        beginTurnState.startup(modelTest);
+        assertEquals(1, modelTest.getModelRep().getActivePlayer());
+        beginTurnState.startup(modelTest);
+        assertEquals (2, modelTest.getModelRep().getActivePlayer());
+
+
+        modelTest.getTurn().getPlayersList().get(2).setWorkersPosition(b1,b2);
+        modelTest.updateModelRep();
+
+        int [][] workers = modelTest.getModelRep().getWorkerPosition();
+
+        assertEquals(0, workers[1][2]);
+        assertEquals(-1, workers[0][2]);
+        assertEquals(0, workers[0][3]);
+
+        modelTest.getTurn().getPlayersList().get(2).setWorkersPosition(b3,b4);
+        modelTest.updateModelRep();
+        int [][] workers1 = modelTest.getModelRep().getWorkerPosition();
+
+        assertEquals(0, workers1[2][2]);
+        assertEquals(0, workers1 [3][1]);
+        assertEquals(-1, workers1 [1][1]);
+
+        b5.build();
+        modelTest.updateModelRep();
+        int [][] towers = modelTest.getModelRep().getTowerPosition();
+        assertEquals(1, towers[4][1]);
+
+
+
     }
 }
