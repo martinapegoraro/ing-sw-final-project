bf1e1fa24deb3d5f9d6bcebc7b12cfd688f93142
martinapegoraro <61741320+martinapegoraro@users.noreply.github.com>
2020-05-10 13:04:24 +0200

    Aggiornato CheckWinConditionState (da sistemare)

--- GC30ProgettoFinale/src/main/java/it/polimi/ingsw/Controller/CheckWinConditionState.java
+++ GC30ProgettoFinale/src/main/java/it/polimi/ingsw/Controller/CheckWinConditionState.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Controller;
 
+import it.polimi.ingsw.Model.GodsList;
 import it.polimi.ingsw.Model.Model;
 import it.polimi.ingsw.Utils.Choice;
 
@@ -18,11 +19,44 @@ public class CheckWinConditionState implements State{
         return stateId;
     }
 
-    @Override
-    public void startup(Model model) {
+
+    public void CheckWinConditionState( Model model)
+    {
+        GodsList currentGod = model.getTurn().getCurrentPlayer().getGod();
+        if(currentGod == GodsList.PAN)
+        {
+            if ()  //TODO: manca il flag per cappire se il worker si è mosso in giù di due livelli
+            {
+               model.getTurn().getCurrentPlayer().setHasWon();
+            }
+        }
+        int playerNumber = model.getTurn().getPlayersList().size();
+        for (int i = 0; i< playerNumber; i ++)
+        {
+            if(model.getTurn().getPlayersList().get(i).getGod() == GodsList.CHRONUS)
+            {
+                if() //TODO: manca il flag per vedere se ci sono 5 torri complete sulla Board
+                {
+                    model.getTurn().getPlayersList().get(i).setHasWon();
+                }
+            }
+        }
+
 
     }
-
+    @Override
+    public void startup(Model model)
+    {
+        int playerNumber = model.getTurn().getPlayersList().size();
+        for (int i = 0; i<playerNumber; i++)
+        {
+            if(model.getTurn().getPlayersList().get(i).getHasWon())
+            {
+                //the game ends
+                hasFinished = true;
+            }
+        }
+    }
     @Override
     public void update(Choice userChoice, Model model) {
 
@@ -32,4 +66,6 @@ public class CheckWinConditionState implements State{
     public boolean hasFinished() {
         return hasFinished;
     }
+
+
 }
